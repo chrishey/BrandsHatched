00f9fa61f2f7f544b4286eb4f776f9de247a25be
DRL\heych <D@rr3nH3lm>
2015-05-28 13:05:33 +0100

    Bring in nancy view base namespace so view loads

--- src/BrandsHatched.Web/Modules/HomeModule.cs
+++ src/BrandsHatched.Web/Modules/HomeModule.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Web;
-using BrandsHatched.Web.ViewModel;
+﻿using BrandsHatched.Web.ViewModel;
 using Nancy;
 
 namespace BrandsHatched.Web.Modules
