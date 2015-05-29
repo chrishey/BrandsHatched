81868bae3c8f9385ece4297381407d834fcc390f
DRL\heych <D@rr3nH3lm>
2015-05-29 16:39:15 +0100

    Added MVC project to start looking at an attribute for the circuit breaker

---
+++ src/BrandsHatched.Mvc/CircuitBreakerAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Web.Mvc;
+
+namespace BrandsHatched.Mvc
+{
+	public class CircuitBreakerAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			
+
+			filterContext.Result = new HttpStatusCodeResult(HttpStatusCode.ServiceUnavailable);
+		}
+	}
+}
