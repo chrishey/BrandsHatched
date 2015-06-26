4115a3b358fc6a05b3637047e28fde25145a33eb
DRL\heych <chris.hey@slicedbread.co.uk>
2015-06-26 11:40:46 +0100

    Add class and interface for collection of broken circuits to check before executing the called method

---
+++ src/BrandsHatched.CircuitBreaker/Store/IBrokenCircuits.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace BrandsHatched.CircuitBreaker.Store
+{
+	public interface IBrokenCircuits
+	{
+		void AddBrokenCircuit(string key, object value);
+		void RemoveBrokenCircuit(string key);
+		bool CircuitBrokenForUser(string key);
+	}
+
+	public class BrokenCircuits : IBrokenCircuits
+	{
+		private Dictionary<string, object> _brokenCircuits;
+
+		public BrokenCircuits()
+		{
+			_brokenCircuits = new Dictionary<string, object>();
+		}
+
+		public void AddBrokenCircuit(string key, object value)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void RemoveBrokenCircuit(string key)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool CircuitBrokenForUser(string key)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
